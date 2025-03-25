b3d4b0fa7ffaffc021c66d54c255121202797dd7
LieDING <ding@getmicco.com>
2025-03-25 18:18:25 +0100

    25 18.18

--- redux-puli-dev.py
+++ redux-puli-dev.py
@@ -7,6 +7,7 @@ from nodes import NODE_CLASS_MAPPINGS
 from comfy import model_management
 from huggingface_hub import hf_hub_download
 from extra_config import load_extra_path_config as _load_extra_path_config
+from nodes import CheckpointLoaderSimple, LoadImage
 
 def get_value_at_index(obj: Union[Sequence, Mapping], index: int) -> Any:
     """Returns the value at the given index of a sequence or mapping.
@@ -124,46 +125,48 @@ def main():
     
     import_custom_nodes()
     with torch.inference_mode():
-        CheckpointLoaderSimple = NODE_CLASS_MAPPINGS["CheckpointLoaderSimple"]
-        LoadImage = NODE_CLASS_MAPPINGS["LoadImage"]
-        PulidFluxModelLoader = NODE_CLASS_MAPPINGS["PulidFluxModelLoader"]
-        PulidFluxEvaClipLoader = NODE_CLASS_MAPPINGS["PulidFluxEvaClipLoader"]
-        PulidFluxInsightFaceLoader = NODE_CLASS_MAPPINGS["PulidFluxInsightFaceLoader"]
-        ApplyPulidFlux = NODE_CLASS_MAPPINGS["ApplyPulidFlux"]
+        loader = CheckpointLoaderSimple()
+        model, clipencoder, vae = loader.load_checkpoint(ckpt_name="flux1-dev-fp8.safetensors")
+        img_loader = LoadImage()
 
-        checkpointLoaderSimple = CheckpointLoaderSimple()
-        ckpt = checkpointLoaderSimple.load_checkpoint(
-            ckpt_name="flux1-dev-fp8.safetensors"
-        )
-        loadimage = LoadImage()
-        reference_loadimage = loadimage.load_image(image="reference.jpg")
+        style_img = img_loader.load_image(image="DUJARDIN-JEAN-AC-PP-7994-PS-03-A_LOGO-822x1024.jpg")
 
-        pulidfluxmodelloader = PulidFluxModelLoader()
-        pulidfluxmodelloader_19 = pulidfluxmodelloader.load_model(
-            pulid_file="pulid_flux_v0.9.1.safetensors"
+
+        StyleModelLoader = NODE_CLASS_MAPPINGS["StyleModelLoader"]
+        CLIPVisionLoader = NODE_CLASS_MAPPINGS["CLIPVisionLoader"]
+
+        stylemodelloader = StyleModelLoader()
+        stylemodelloader_28 = stylemodelloader.load_style_model(
+            style_model_name="flux1-redux-dev"
         )
 
-        pulidfluxevacliploader = PulidFluxEvaClipLoader()
-        pulidfluxevacliploader_21 = pulidfluxevacliploader.load_eva_clip()
-
-        pulidfluxinsightfaceloader =PulidFluxInsightFaceLoader()
-        pulidfluxinsightfaceloader_22 = pulidfluxinsightfaceloader.load_insightface(
-            provider="CUDA"
+        clipvisionloader = CLIPVisionLoader()
+        clipvisionloader_29 = clipvisionloader.load_clip(
+            clip_name="sigclip_vision_patch14_384.safetensors"
         )
 
-        applypulidflux = ApplyPulidFlux()
-        applypulidflux_20 = applypulidflux.apply_pulid_flux(
-            weight=0.9500000000000001,
-            start_at=0,
-            end_at=1,
-            model=get_value_at_index(ckpt, 0),
-            pulid_flux=get_value_at_index(pulidfluxmodelloader_19, 0),
-            eva_clip=get_value_at_index(pulidfluxevacliploader_21, 0),
-            face_analysis=get_value_at_index(pulidfluxinsightfaceloader_22, 0),
-            image=get_value_at_index(reference_loadimage, 0),
-            unique_id=1774265400529356555,
+        reduxadvanced = NODE_CLASS_MAPPINGS["ReduxAdvanced"]()
+
+        cliptextencode_32 = clipencoder.encode(text="Ultra realistic. Masterpiece. This black-and-white photograph, likely taken with a high-resolution DSLR camera using a medium aperture (f/5.6), captures man in a close-up portrait. He is neatly combed, short hair, and a slight smile, wears a formal suit and tie. The lighting is dramatic, with a spotlight creating a halo effect around his face, casting shadows that highlight his facial features. The background is dark, emphasizing the subject. ",)
+
+        reduxadvanced_24 = reduxadvanced.apply_stylemodel(
+            downsampling_factor=1,
+            downsampling_function="nearest",
+            mode="keep aspect ratio",
+            weight=0.5,
+            autocrop_margin=0.1,
+            conditioning=get_value_at_index(cliptextencode_32, 0),
+            style_model=get_value_at_index(stylemodelloader_28, 0),
+            clip_vision=get_value_at_index(clipvisionloader_29, 0),
+            image=get_value_at_index(style_img, 0),
         )
-        print(get_value_at_index(applypulidflux_20, 0))
+
+        redux_output = get_value_at_index(reduxadvanced_24, 0)
+
+        print(redux_output, redux_output.shape)
+
+
+        
 
 
 def __main():
