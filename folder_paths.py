7e32d90efe9136831fde9ea4a139c6a794e2d286
LieDING <ding@getmicco.com>
2025-03-26 20:23:17 +0100

    26 20.23

--- folder_paths.py
+++ folder_paths.py
@@ -291,7 +291,7 @@ def get_full_path(folder_name: str, filename: str) -> str | None:
 def get_full_path_or_raise(folder_name: str, filename: str) -> str:
     full_path = get_full_path(folder_name, filename)
     if full_path is None:
-        raise FileNotFoundError(f"Model in folder '{folder_name}' with filename '{filename}' not found.")
+        raise FileNotFoundError(f"Model in folder '{folder_name}' with filename '{filename}', '{full_path}', not found.")
     return full_path
 
 
